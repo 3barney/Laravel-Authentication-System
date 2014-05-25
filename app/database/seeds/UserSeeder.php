aebe914203def57d9e17a7f3947ade1c2514b10e
3barney <brnbsjm@gmail.com>
2014-05-25 11:29:43 +0300

    users seeder to emulate user data

---
+++ app/database/seeds/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barney
+ * Date: 5/24/14
+ * Time: 1:44 PM
+ */
+class UserSeeder extends DatabaseSeeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                "username" => "Barnabas Jomo",
+                "password" => Hash::make("195960c23"),
+                "email" => "brnbsjm@gmail.com"
+            ]
+        ];
+
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
+    }
+}
