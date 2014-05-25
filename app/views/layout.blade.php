f6c09526d49d8c1c239d42f5439b9e8d5e1c7790
3barney <brnbsjm@gmail.com>
2014-05-25 16:38:44 +0300

    main layout template file

---
+++ app/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/layout.css" />
+        <title>Authentication</title>
+    </head>
+
+    <body>
+        @include("header")
+        <div class="content">
+            <div class="container">
+                @yield("content")
+            </div>
+        </div>
+        @include("footer")
+    </body>
+</html>
