b7f51ddf0763434b78a85fc728d060b50b0955bd
3barney <brnbsjm@gmail.com>
2014-05-25 11:36:39 +0300

    db

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'auth_db',
 			'username'  => 'root',
 			'password'  => '195960c23',
 			'charset'   => 'utf8',
