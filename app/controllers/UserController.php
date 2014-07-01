bca298eb04ff752664027c80a176f8ded253f518
3barney <brnbsjm@gmail.com>
2014-07-01 17:58:09 +0300

    users controller

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Barney
- * Date: 5/25/14
- * Time: 5:15 PM
- */ 
+
+class UserController extends BaseController
+{
+    public function loginAction()
+    {
+    /*
+        $data = array();
+
+        if ( Input::server("REQUEST_METHOD") == "POST")
+        {
+            $validator =  Validator::make( Input::all(), ( array(
+                            "username" => "required",
+                            "password" => "required"
+            ) ));
+        }
+
+        if ($validator->passes() )
+        {
+
+        }
+        else
+        {
+            $data["errors"] = new MessageBag(
+                array("password" => "username and/or password invalid")
+            );
+        }
+        //blah blah blah, make sure you delete
+    */
+        return View::make("user/login");
+    }
+}
